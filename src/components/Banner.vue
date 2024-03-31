770c2d54538a8e9af25c30f83b220ca19708e244
Davi Gledson <davigledson@outlook.com>
2024-03-31 08:30:05 -0300

    Criando banner da aplicação

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="main-banner">
+        <h1>
+            make your Burger
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "Banner",
+}
+</script>
+<style scoped>
+
+#main-banner {
+    background-image: url("/img/burger.jpg");
+    background-position: 0 - 250px;
+    background-size: cover;
+    display: flex;
+    height: 500px;
+    align-items: center;
+    justify-content: flex-start;
+
+}
+#main-banner h1 {
+    color: #fff;
+    text-align: center;
+    font-size: 60px;
+    background-color: #222;
+    padding: 20px;
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,22 @@
 
 
 <template>
+  <Banner/>
+  
+  <p>banner</p>
   <main class="main-container">
- <h1>HOME</h1>
+ <h1>Monte o seu burger</h1>
+ <p>formulario</p>
   </main>
 </template>
+
+<script>
+import Banner from "../components/Banner.vue"
+
+export default {
+  name: "Home",
+  components: {
+    Banner
+  }
+}
+</script>
