18821d108ada83a8c95a3141bac2caba82404da0
Davi Gledson <davigledson@outlook.com>
2024-04-01 20:05:48 -0300

    Criando a tela de pedidos (Dashboard)

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <h1>Gerenciar PEDIDOS</h1>
+    <Dashboard/>
   </div>
 </template>
 
+<script>
+import Dashboard from '../components/DashBoard.vue'
+export default {
+  name:"Pedidos",
+  components: {
+    Dashboard
+  }
+}
+
+
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
